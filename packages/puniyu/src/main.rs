469cd8220e88d77898cc82944c91d75479c4665f
wuliya <3369906077@qq.com>
2025-11-23 23:11:26 +0800

    chore(cargo): 更新配置以支持本地包路径依赖

--- packages/puniyu/src/main.rs
+++ packages/puniyu/src/main.rs
@@ -3,6 +3,9 @@ use puniyu_core::AppBuilder;
 #[tokio::main]
 async fn main() {
 	const LOGO: &[u8] = include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/assets/logo.png"));
-	let app = AppBuilder::new().with_adapter(&puniyu_adapter_console::Adapter).with_logo(LOGO.to_vec()).build();
+	let app = AppBuilder::new()
+		.with_logo(LOGO.to_vec())
+		.with_adapter(&puniyu_adapter_console::Adapter)
+		.build();
 	app.run().await;
 }
