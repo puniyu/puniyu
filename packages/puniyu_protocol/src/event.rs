09930b7d6ca1b1b5e2d9e04726eede0c3c02ad44
wuliya <3369906077@qq.com>
2025-12-27 11:39:43 +0800

    refactor(protocol): 重构事件协议结构
    - 创建新的 event/message.proto 文件用于统一管理消息事件
    - 分离接收和发送消息的协议定义到独立的 proto 文件中
    - 更新 Rust 代码中的类型转换实现以适配新的协议结构
    - 修改事件处理的模块组织结构以支持双向消息传输
    - 重构消息事件的枚举定义以区分接收和发送类型

--- packages/puniyu_protocol/src/event.rs
+++ packages/puniyu_protocol/src/event.rs
@@ -3,24 +3,24 @@ use puniyu_types::event as puniyu_event;
 use puniyu_types::event::message as puniyu_message;
 include!(concat!(env!("OUT_DIR"), "/puniyu.event.rs"));
 
-impl From<event::Event> for puniyu_types::event::Event {
-	fn from(event: event::Event) -> Self {
+impl From<event_receive::Event> for puniyu_event::Event {
+	fn from(event: event_receive::Event) -> Self {
 		match event {
-			event::Event::MessageEvent(message) => {
+			event_receive::Event::MessageEvent(message) => {
 				let message: puniyu_message::MessageEvent =
-					puniyu_message::MessageEvent::from(*message);
+					puniyu_message::MessageEvent::from(message);
 				Self::Message(Box::new(message))
 			}
 		}
 	}
 }
 
-impl From<puniyu_event::Event> for event::Event {
+impl From<puniyu_event::Event> for event_receive::Event {
 	fn from(event: puniyu_event::Event) -> Self {
 		match event {
 			puniyu_event::Event::Message(message) => {
 				let message: puniyu_message::MessageEvent = *message;
-				Self::MessageEvent(Box::new(message.into()))
+				Self::MessageEvent(message.into())
 			}
 			_ => panic!("Unsupported event type"),
 		}
--- packages/puniyu_protocol/src/event/message.rs
+++ packages/puniyu_protocol/src/event/message.rs
@@ -1,127 +1,55 @@
-use super::{FriendMessage, GroupMessage, MessageEvent, message_event};
-use puniyu_types::event::message::{MessageBase, MessageBuilder};
-use puniyu_types::event::{EventBase, message};
+pub mod send;
+pub mod receive;
 
-impl From<FriendMessage> for message::FriendMessage {
-	fn from(message: FriendMessage) -> Self {
-		let builder = MessageBuilder {
-			bot: message.friend_message_bot.unwrap().into(),
-			event_id: message.event_id,
-			self_id: message.self_id,
-			user_id: message.user_id,
-			contact: message.contact.unwrap().into(),
-			sender: message.sender.unwrap().into(),
-			time: message.time,
-			message_id: message.message_id,
-			elements: message
-				.elements
-				.into_iter()
-				.map(|element| element.into())
-				.collect::<Vec<_>>(),
-		};
-		Self::new(builder)
-	}
+include!(concat!(env!("OUT_DIR"), "/puniyu.event.message.rs"));
+use puniyu_types::event::message as puniyu_message;
+
+
+impl From<message_event_receive::MessageEvent> for puniyu_message::MessageEvent {
+    fn from(message: message_event_receive::MessageEvent) -> Self {
+        match message {
+            message_event_receive::MessageEvent::FriendMessage(friend) => {
+                puniyu_message::MessageEvent::Friend(friend.into())
+            }
+            message_event_receive::MessageEvent::GroupMessage(group) => {
+                puniyu_message::MessageEvent::Group(group.into())
+            }
+        }
+    }
 }
 
-impl From<message::FriendMessage> for FriendMessage {
-	fn from(message: message::FriendMessage) -> Self {
-		let bot = message.bot().clone();
-		Self {
-			friend_message_bot: Some(bot.into()),
-			event_id: message.event_id().to_string(),
-			time: message.time(),
-			self_id: message.self_id().to_string(),
-			user_id: message.user_id().to_string(),
-			message_id: message.message_id().to_string(),
-			elements: message
-				.elements()
-				.into_iter()
-				.map(|element| element.into())
-				.collect::<Vec<_>>(),
-			contact: Some(message.contact().into()),
-			sender: Some(message.sender().into()),
-		}
-	}
-}
-
-impl From<GroupMessage> for message::GroupMessage {
-	fn from(message: GroupMessage) -> Self {
-		let builder = MessageBuilder {
-			bot: message.group_message_bot.unwrap().into(),
-			event_id: message.event_id,
-			self_id: message.self_id,
-			user_id: message.user_id,
-			contact: message.contact.unwrap().into(),
-			sender: message.sender.unwrap().into(),
-			time: message.time,
-			message_id: message.message_id,
-			elements: message
-				.elements
-				.into_iter()
-				.map(|element| element.into())
-				.collect::<Vec<_>>(),
-		};
-		Self::new(builder)
-	}
-}
-
-impl From<message::GroupMessage> for GroupMessage {
-	fn from(message: message::GroupMessage) -> Self {
-		let bot = message.bot().clone();
-		Self {
-			group_message_bot: Some(bot.into()),
-			event_id: message.event_id().to_string(),
-			time: message.time(),
-			self_id: message.self_id().to_string(),
-			user_id: message.user_id().to_string(),
-			message_id: message.message_id().to_string(),
-			elements: message
-				.elements()
-				.into_iter()
-				.map(|element| element.into())
-				.collect::<Vec<_>>(),
-			contact: Some(message.contact().into()),
-			sender: Some(message.sender().into()),
-		}
-	}
-}
-
-impl From<message_event::MessageEvent> for message::MessageEvent {
-	fn from(message: message_event::MessageEvent) -> Self {
-		match message {
-			message_event::MessageEvent::FriendMessage(friend) => {
-				message::MessageEvent::Friend(friend.into())
-			}
-			message_event::MessageEvent::GroupMessage(group) => {
-				message::MessageEvent::Group(group.into())
-			}
-		}
-	}
-}
-
-impl From<message::MessageEvent> for message_event::MessageEvent {
-	fn from(message: message::MessageEvent) -> Self {
-		match message {
-			message::MessageEvent::Friend(friend) => {
-				message_event::MessageEvent::FriendMessage(friend.into())
-			}
-			message::MessageEvent::Group(group) => {
-				message_event::MessageEvent::GroupMessage(group.into())
-			}
-		}
-	}
-}
-
-impl From<MessageEvent> for message::MessageEvent {
-	fn from(message: MessageEvent) -> Self {
-		let message = message.message_event.unwrap();
-		message.into()
-	}
+impl From<puniyu_message::MessageEvent> for message_event_receive::MessageEvent {
+    fn from(message: puniyu_message::MessageEvent) -> Self {
+        match message {
+            puniyu_message::MessageEvent::Friend(friend) => {
+                message_event_receive::MessageEvent::FriendMessage(friend.into())
+            }
+            puniyu_message::MessageEvent::Group(group) => {
+                message_event_receive::MessageEvent::GroupMessage(group.into())
+            }
+        }
+    }
 }
 
 
-impl From<message::MessageEvent> for MessageEvent {
-	fn from(message: message::MessageEvent) -> Self {
-		Self { message_event: Some(message.into()) }
-	}
+impl From<MessageEventReceive> for puniyu_message::MessageEvent {
+    fn from(message: MessageEventReceive) -> Self {
+        match message.message_event.unwrap() {
+            message_event_receive::MessageEvent::FriendMessage(message) => {
+                puniyu_message::MessageEvent::Friend(message.into())
+            }
+            message_event_receive::MessageEvent::GroupMessage(message) => {
+                puniyu_message::MessageEvent::Group(message.into())
+            }
+        }
+    }
 }
+
+
+
+impl From<puniyu_message::MessageEvent> for MessageEventReceive {
+    fn from(message: puniyu_message::MessageEvent) -> Self {
+        Self { message_event: Some(message.into()) }
+    }
+}
+
