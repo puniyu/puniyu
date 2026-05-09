2ee2be55d52bae40c3ab1e3f1d3b22c4774b3953
wuliya <3369906077@qq.com>
2026-05-09 10:30:00 +0800

    chore: 修复资源路径

--- puniyu/build.rs
+++ puniyu/build.rs
@@ -1,10 +1,18 @@
 #![allow(clippy::unwrap_used)]
 
 use vergen_git2::{BuildBuilder, Emitter, Git2Builder};
+use cargo_metadata::MetadataCommand;
 
 fn main() {
 	println!("cargo:rerun-if-changed=build.rs");
-	println!("cargo:rerun-if-changed=../../.git/HEAD");
+	println!("cargo:rerun-if-changed=../.git/HEAD");
+
+	let workspace_dir = MetadataCommand::new()
+		.current_dir(env!("CARGO_MANIFEST_DIR"))
+		.exec()
+		.unwrap()
+		.workspace_root;
+	println!("cargo:rustc-env=WORKSPACE_DIR={workspace_dir}");
 	let build = BuildBuilder::all_build().unwrap();
 	let git = Git2Builder::all_git().unwrap();
 	Emitter::default()
@@ -13,5 +21,5 @@ fn main() {
 		.add_instructions(&git)
 		.unwrap()
 		.emit()
-		.unwrap()
+		.unwrap();
 }
